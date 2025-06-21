d44feb71a26a190eb1a6c38cdd6d16110097cb6f
Sven Kroflin <99004305+skroflin@users.noreply.github.com>
2025-06-21 13:48:12 +0200

    Kreirane osnovne CRUD metode za Kutiju
    Kreirane create, read, update i delete metode za SkroflinKutijaService.

--- src/main/java/ffos/skroflin/service/SkroflinKutijaService.java
+++ src/main/java/ffos/skroflin/service/SkroflinKutijaService.java
@@ -4,6 +4,11 @@
  */
 package ffos.skroflin.service;
 
+import ffos.skroflin.model.SkroflinKutija;
+import ffos.skroflin.model.SkroflinPolica;
+import ffos.skroflin.model.dto.SkroflinKutijaDTO;
+import java.math.BigDecimal;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,5 +17,30 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SkroflinKutijaService extends GlavniService{
-
+    public List<SkroflinKutija> getAll(){
+        return session.createQuery("from skroflin_kutija", SkroflinKutija.class).list();
+    }
+    
+    public SkroflinKutija getBySifra(int sifra){
+        return session.get(SkroflinKutija.class, sifra);
+    }
+    
+    public SkroflinKutija post(SkroflinKutijaDTO o){
+        session.beginTransaction();
+        SkroflinKutija sk;
+        SkroflinPolica skroflinPolica = session.get(SkroflinPolica.class, o.policaSifra());
+        sk = new SkroflinKutija(o.datumPohrane(), o.obujam(), o.oznakaKutije(), skroflinPolica);
+        session.persist(sk);
+        session.getTransaction().commit();
+        return sk;
+    }
+    
+    public void put(SkroflinKutijaDTO o, int sifra){
+        session.beginTransaction();
+        SkroflinKutija sk = (SkroflinKutija) session.get(SkroflinKutija.class, sifra);
+        SkroflinPolica skroflinPolica = session.get(SkroflinPolica.class, o.policaSifra());
+        sk = new SkroflinKutija(o.datumPohrane(), o.obujam(), o.oznakaKutije(), skroflinPolica);
+        session.persist(sk);
+        session.getTransaction().commit();
+    }
 }
